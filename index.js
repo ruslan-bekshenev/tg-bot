767073f952125238d6a26bc6d13e0638225b8718
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-03-26 22:17:34 +0500

    test bot

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+
+const TelegramApi = require('node-telegram-bot-api')
+const token = `${process.env.BOT_ID}:${process.env.BOT_TOKEN}`
+
+const bot = new TelegramApi(token, { polling: true })
+
+const chats = {}
+
+const startGame = async (chatId) => {
+  await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать`)
+  const randomNumber = Math.floor(Math.random() * 10)
+  chats[chatId] = randomNumber;
+  await bot.sendMessage(chatId, 'Отгадывай', gameOptions)
+}
+const start = () => {
+  bot.setMyCommands([
+    { command: '/start', description: 'Приветствие' },
+    { command: '/info', description: 'Получить информацию' },
+    { command: '/kto_loh', description: 'Определяет лоха' },
+    { command: '/goroscope', description: 'Узнай свою судьбу на сегодня' },
+    { command: '/game', description: 'Просто какая-то игра' },
+  ])
+  
+  bot.on('message', async msg => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    if (text === '/start') {
+      return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот`)
+    }
+    if (text === '/info') {
+      return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`)
+    }
+    if (text === '/kto_loh') {
+      console.log(msg)
+      return bot.sendMessage(chatId, `Ты ${msg.from.first_name} ${msg.from.last_name}`)
+    }
+    return bot.sendMessage(chatId, 'Не понимать')
+  })
+  
+  bot.on('callback_query', msg => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id
+    if (data === '/again') {
+      return startGame(chatId)
+    }
+    if (data === chats[chatId]) {
+      return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions)
+    } else {
+      return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions)
+    }
+  })
+}
+
+start()
